6cf9f4e22ddf0046ff1ba900ac8f9ea1cdb51661
Colin Chartier <colin.chartier@sentry.io>
2025-01-14 19:13:29 -0500

    undo rust/src/lib

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,24 +1,6 @@
-pub mod snuba {
-    pub mod v1 {
-       tonic::include_proto!("sentry_protos.snuba.v1");
-   }
-}
-
 pub mod events {
     pub mod v1 {
        tonic::include_proto!("sentry_protos.kafka.events.v1");
-   }
-}
-
-pub mod sentry {
-    pub mod v1 {
-       tonic::include_proto!("sentry_protos.sentry.v1");
-   }
-}
-
-pub mod relay {
-    pub mod v1 {
-       tonic::include_proto!("sentry_protos.relay.v1");
    }
 }
 
@@ -31,6 +13,21 @@ pub mod options {
 pub mod seer {
     pub mod v1 {
        tonic::include_proto!("sentry_protos.seer.v1");
+   }
+}
+
+pub mod snuba {
+    pub mod v1 {
+       tonic::include_proto!("sentry_protos.snuba.v1");
+   }
+    pub mod v1alpha {
+       tonic::include_proto!("sentry_protos.snuba.v1alpha");
+   }
+}
+
+pub mod relay {
+    pub mod v1 {
+       tonic::include_proto!("sentry_protos.relay.v1");
    }
 }
 
