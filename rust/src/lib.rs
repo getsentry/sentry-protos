fca5f8612788e7d56f530278874dcb0288292c9a
Enoch Tang <enoch.tang@sentry.io>
2025-01-20 12:02:00 -0500

    simplify retrystate to use attempts_remaining and on_attempts_exceeded

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,9 +4,9 @@ pub mod events {
    }
 }
 
-pub mod options {
+pub mod relay {
     pub mod v1 {
-       tonic::include_proto!("sentry_protos.options.v1");
+       tonic::include_proto!("sentry_protos.relay.v1");
    }
 }
 
@@ -16,18 +16,21 @@ pub mod seer {
    }
 }
 
-pub mod snuba {
+pub mod options {
     pub mod v1 {
-       tonic::include_proto!("sentry_protos.snuba.v1");
-   }
-    pub mod v1alpha {
-       tonic::include_proto!("sentry_protos.snuba.v1alpha");
+       tonic::include_proto!("sentry_protos.options.v1");
    }
 }
 
-pub mod relay {
+pub mod sentry {
     pub mod v1 {
-       tonic::include_proto!("sentry_protos.relay.v1");
+       tonic::include_proto!("sentry_protos.sentry.v1");
+   }
+}
+
+pub mod snuba {
+    pub mod v1 {
+       tonic::include_proto!("sentry_protos.snuba.v1");
    }
 }
 
