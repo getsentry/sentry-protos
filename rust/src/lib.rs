15ed71fff22a31251d426f60f826b76f3dff7111
John <john.yang@sentry.io>
2025-04-09 18:20:10 -0400

    feat(taskworker): add optional delay value to ktasks (#117)
    Co-authored-by: Mark Story <mark@mark-story.com>

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,6 +1,12 @@
-pub mod taskbroker {
+pub mod sentry {
     pub mod v1 {
-       tonic::include_proto!("sentry_protos.taskbroker.v1");
+       tonic::include_proto!("sentry_protos.sentry.v1");
+   }
+}
+
+pub mod relay {
+    pub mod v1 {
+       tonic::include_proto!("sentry_protos.relay.v1");
    }
 }
 
@@ -16,27 +22,21 @@ pub mod options {
    }
 }
 
-pub mod relay {
-    pub mod v1 {
-       tonic::include_proto!("sentry_protos.relay.v1");
-   }
-}
-
 pub mod events {
     pub mod v1 {
        tonic::include_proto!("sentry_protos.kafka.events.v1");
    }
 }
 
-pub mod sentry {
-    pub mod v1 {
-       tonic::include_proto!("sentry_protos.sentry.v1");
-   }
-}
-
 pub mod snuba {
     pub mod v1 {
        tonic::include_proto!("sentry_protos.snuba.v1");
+   }
+}
+
+pub mod taskbroker {
+    pub mod v1 {
+       tonic::include_proto!("sentry_protos.taskbroker.v1");
    }
 }
 
