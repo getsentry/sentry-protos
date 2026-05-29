fb3d43b7ad77aff60a3f4294ddcab4e8f59c88f0
Brendan Hy <brendan.hy@sentry.io>
2026-05-29 14:09:38 -0700

    feat(billing-platform): Add uncapped_rate to LineItemConfig (#280)
    * feat(billing-platform): Add uncapped_rate to LineItemConfig
    
    * chore: Regenerate Rust bindings
    
    ---------
    
    Co-authored-by: getsantry[bot] <66042841+getsantry[bot]@users.noreply.github.com>

--- rust/src/sentry_protos.billing.v1.services.package.v1.rs
+++ rust/src/sentry_protos.billing.v1.services.package.v1.rs
@@ -20,6 +20,11 @@ pub struct LineItemConfig {
     /// Whether this LineItem is included in the base package or not (ie Seer)
     #[prost(bool, tag = "8")]
     pub is_optional_add_on: bool,
+    /// Used for calculating spend that should not apply towards an organization's PAYG budget (ie Seer seats)
+    #[prost(message, optional, tag = "9")]
+    pub uncapped_rate: ::core::option::Option<
+        super::super::super::common::v1::TieredPricingRate,
+    >,
     /// how many of this lineitem are included in the package, the customer can reserve more on their contract
     #[prost(oneof = "line_item_config::IncludedReservedUnits", tags = "5, 6")]
     pub included_reserved_units: ::core::option::Option<
