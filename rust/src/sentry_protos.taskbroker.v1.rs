f52b95de5aad23d8c62965826d16063919767c8a
james-mcnulty <george.berdovskiy@sentry.io>
2026-07-28 07:05:40 -0700

    Remove Callback URL Field

--- rust/src/sentry_protos.taskbroker.v1.rs
+++ rust/src/sentry_protos.taskbroker.v1.rs
@@ -137,8 +137,6 @@ pub struct SetBatchActivationStatusResponse {}
 pub struct PushTaskRequest {
     #[prost(message, optional, tag = "1")]
     pub task: ::core::option::Option<TaskActivation>,
-    #[prost(string, tag = "2")]
-    pub callback_url: ::prost::alloc::string::String,
 }
 #[derive(Clone, Copy, PartialEq, Eq, Hash, ::prost::Message)]
 pub struct PushTaskResponse {}
