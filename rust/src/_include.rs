4c76e6618964fe3053fa046a350d69208ab38394
Brendan Hy <brendan.hy@sentry.io>
2026-09-17 13:46:00 -0700

    chore(billing-platform): Add snapshot service protos (#429)
    * chore(billing-platform): Add snapshot service protos
    
    * chore: Regenerate Rust bindings
    
    * chore: Regenerate Rust bindings
    
    * fix
    
    * chore: Regenerate Rust bindings
    
    * chore: Regenerate Rust bindings
    
    * chore: Regenerate Rust bindings
    
    ---------
    
    Co-authored-by: getsantry[bot] <66042841+getsantry[bot]@users.noreply.github.com>

--- rust/src/_include.rs
+++ rust/src/_include.rs
@@ -42,6 +42,11 @@ pub mod sentry_protos {
                         include!("sentry_protos.billing.v1.services.contract_budget_enforcer.v1.rs");
                     }
                 }
+                pub mod contract_usage_snapshot {
+                    pub mod v1 {
+                        include!("sentry_protos.billing.v1.services.contract_usage_snapshot.v1.rs");
+                    }
+                }
                 pub mod engagement {
                     pub mod v1 {
                         include!("sentry_protos.billing.v1.services.engagement.v1.rs");
